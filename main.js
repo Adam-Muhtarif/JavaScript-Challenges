6f099de9b12fb762093690ae8c98e17f9c87ff9e
Adam Muhtarif <muhtarif.dev.web@gmail.com>
2023-01-12 00:26:52 -0800

    vs

--- main.js
+++ main.js
@@ -1353,4 +1353,4 @@ document.write(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
 // Hello Ahmed A., Your Age Is 32, You Live In SY
 
 // return zName.substring(0, 6) + zName.substring(6, 7) + ".";
-// new test
+// this comment commit by vs
