5182cc5784918fd955665db3a9b9d1cbbbc3072e
Adam Abdulkarim Adam <106653475+Adam-Muhtarif@users.noreply.github.com>
2023-02-24 00:33:29 -0800

    add challenge 21

--- js/main.js
+++ js/main.js
@@ -3800,3 +3800,58 @@ if (holidays === 0) {
   // Needed Output
   // My Best Friends: Shady, Mahmoud, Ahmed
 */
+
+//? ------------ Challenge 21 ---------------------
+/* 
+`
+
+`;
+//----------------  Solution--------------------------
+  const game = {
+    title: "YS",
+    developer: "Falcom",
+    releases: {
+      "Oath In Felghana": ["USA", "Japan"],
+      "Ark Of Napishtim": {
+        US: "20 USD",
+        JAP: "10 USD",
+      },
+      Origin: "30 USD",
+    },
+  };
+
+  // Write Your Destructuring Assignment/s Here
+
+  let { title: t, developer: d } = game;
+  let {
+    o = ["Oath In Felghana"],
+    ["Oath In Felghana"]: [u, j],
+    a = ["Ark Of Napishtim"],
+    ["Ark Of Napishtim"]: { US: u_price },
+    ["Ark Of Napishtim"]: { JAP: j_price },
+    Origin: or,
+  } = game.releases;
+
+  // Needed Output
+
+  console.log(`My Favourite Games Style Is ${t} Style`);
+  // My Favourite Games Style Is YS Style
+
+  console.log(`And I Love ${d} Games`);
+  // And I Love Falcom Games
+
+  console.log(`My Best Release Is ${o} It Released in ${u} & ${j}`);
+  // My Best Release Is Oath In Felghana It Released in USA & Japan
+
+  console.log(`Although I Love ${a}`);
+  // Although I Love Ark Of Napishtim
+
+  console.log(`${a} Price in USA Is ${u_price}`);
+  // Ark Of Napishtim Price in USA Is 20 USD
+
+  console.log(`${a} Price in Japan Is ${j_price}`);
+  // Ark Of Napishtim Price in Japan Is 10 USD
+
+  console.log(`Origin Price Is ${or}`);
+  // Origin Price Is 30 USD
+*/
